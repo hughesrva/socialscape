5e17dc658e0bc78c8ed27f2d255b7cf6e9a5bbd2
skiplodem <hughesrva@gmail.com>
2019-01-05 22:02:19 -0500

    adding function to push results to page

--- assets/js/home.js
+++ assets/js/home.js
@@ -31,6 +31,21 @@ $("body").on("click", ".testButton", function () {
             let event = response.events.event[i];
             // console logs each value from response
             console.log("title "+event.title, "start "+event.start_time, "end "+event.stop_time, "venue "+event.venue_name, "address "+event.venue_address, "url "+event.url);
+
+            var eventCard = $("<div>").addClass("card");
+            var eventContent = $("<div>").addClass("card-content").appendTo(eventCard);
+            var eventTop = $("<div>").addClass("media").appendTo(eventContent);
+            var eventImgPosition = $("<div>").addClass("media-left").appendTo(eventTop);
+            var eventImgHolder = $("<figure>").addClass("image").addClass("is-48x48").appendTo(eventImgPosition);
+            var eventImg = $("<img>").attr("src", "assets/images/logo.png").attr("id", "eventIcon").appendTo(eventImgHolder); //event icon 
+            var eventTitlePosition = $("<div>").addClass("media-content").appendTo(eventTop);
+            var eventLink = $("<a>").attr("href", event.url).appendTo(eventTitlePosition); //event link
+            var eventTitle = $("<p>").addClass("title").addClass("is-4").text(event.title).attr("id", "eventTitle").appendTo(eventLink); //event title
+
+            var eventVenue = $("<div>").addClass("content").text(event.venue_name).attr("id", "eventVenue").appendTo(eventContent); //event venue
+            var eventTime = $("<div>").addClass("content").text(event.start_time).attr("id", "eventTime").appendTo(eventContent); //event time
+            $("#resultsContainer").append(eventCard);
+
         };
     });
     // url: "https://app.ticketmaster.com/discovery/v2/events.json?keyword=music&source=universe&countryCode=US&city=Richmond&apikey=LoeeX4dO34SV1Xl7x72AzwTzSkHRVG0u",
