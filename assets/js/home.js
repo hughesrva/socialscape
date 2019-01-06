9417d3c2621d44ce37cc67beb6d3cfeb36509309
ianschu <ianschu7@gmail.com>
2019-01-05 20:53:10 -0500

    more eventful values

--- assets/js/home.js
+++ assets/js/home.js
@@ -21,14 +21,16 @@ $("body").on("click", ".testButton", function () {
     console.log("clicked");
     // let interests = something from storage;
     $.ajax({
-        url: eventful.queryURL("art"),
+        url: eventful.queryURL("music"),
         dataType: "jsonp",
         method: "GET"
     }).then(function(response){
         console.log(response);
         for (var i = 0; i<response.events.event.length; i++) {
-            // console logs titles of events
-            console.log(response.events.event[i].title);
+            // set variable to clean up code
+            let event = response.events.event[i];
+            // console logs each value from response
+            console.log("title "+event.title, "start "+event.start_time, "end "+event.stop_time, "venue "+event.venue_name, "address "+event.venue_address, "url "+event.url);
         };
     });
     // url: "https://app.ticketmaster.com/discovery/v2/events.json?keyword=music&source=universe&countryCode=US&city=Richmond&apikey=LoeeX4dO34SV1Xl7x72AzwTzSkHRVG0u",
