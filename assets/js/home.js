e85f8181b9b95bac5bc7047bdd9d8f61f7917e8b
skiplodem <hughesrva@gmail.com>
2019-01-08 19:44:19 -0500

    corrected function to show on page

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,45 +1,37 @@
-if (localStorage.getItem("city") === null) {
-    var city = "Richmond, VA";
-}
-else {
-    var city = localStorage.getItem("city");
-}
-
-eventful= {
-    api_key: "app_key=V8VVQZh9Ghmf7bGQ",
-    end: "http://api.eventful.com/json/events/search?",
-    city: city,
-    queryURL: function (search) {
-       url = this.end + this.api_key + "&category=" + search + "&location=" + this.city + "&date=Future";
-        return url;
-    },
-};
-ticketmaster= {
-    api_key: "&apikey=LoeeX4dO34SV1Xl7x72AzwTzSkHRVG0u",
-    end: "https://app.ticketmaster.com/discovery/v2/events.json?",
-    city: city,
-    queryURL: function (search) {
-        url = this.end + "keyword=" + search +"&source=universe&countryCode=US" + "&city="+this.city + this.api_key;
-        return url;
-    }
-}
-
 $("body").on("click", ".testButton", function () {
     console.log("clicked");
-    // let interests = something from storage;
 
+    // sets city to Richmond if one isn't saved in storage
+    if (localStorage.getItem("city") === null) {
+        var city = "Richmond, VA";
+    }
+    else {
+        var city = localStorage.getItem("city");
+    }
+    console.log(city);
+
+    // eventful URL
+    eventful = {
+        api_key: "app_key=V8VVQZh9Ghmf7bGQ",
+        end: "http://api.eventful.com/json/events/search?",
+        city: city,
+        queryURL: function (search) {
+            url = this.end + this.api_key + "&category=" + search + "&location=" + this.city + "&date=Future";
+            return url;
+        },
+    };
 
     $.ajax({
         url: eventful.queryURL(localStorage.getItem("selInts").toString()),
         dataType: "jsonp",
         method: "GET"
-    }).then(function(response){
+    }).then(function (response) {
         console.log(response);
-        for (var i = 0; i<response.events.event.length; i++) {
+        for (var i = 0; i < response.events.event.length; i++) {
             // set variable to clean up code
-            let event = response.events.event[i];
+            let eventResult = response.events.event[i];
             // console logs each value from response
-            console.log("title "+event.title, "start "+event.start_time, "end "+event.stop_time, "venue "+event.venue_name, "address "+event.venue_address, "url "+event.url);
+            // console.log("title "+event.title, "start "+event.start_time, "end "+event.stop_time, "venue "+event.venue_name, "address "+event.venue_address, "url "+event.url);
 
             // creates elements for response data and pushes to page
             var eventCard = $("<div>").addClass("card");
@@ -49,26 +41,12 @@ $("body").on("click", ".testButton", function () {
             var eventImgHolder = $("<figure>").addClass("image").addClass("is-48x48").appendTo(eventImgPosition);
             var eventImg = $("<img>").attr("src", "assets/images/logo.png").attr("id", "eventIcon").appendTo(eventImgHolder); //event icon 
             var eventTitlePosition = $("<div>").addClass("media-content").appendTo(eventTop);
-            var eventLink = $("<a>").attr("href", event.url).appendTo(eventTitlePosition); //event link
-            var eventTitle = $("<p>").addClass("title").addClass("is-4").text(event.title).attr("id", "eventTitle").appendTo(eventLink); //event title
-            var eventVenue = $("<div>").addClass("content").text(event.venue_name).attr("id", "eventVenue").appendTo(eventContent); //event venue
-            var eventTime = $("<div>").addClass("content").text(event.start_time).attr("id", "eventTime").appendTo(eventContent); //event time
-            $("#resultsContainer").append(eventCard);
+            var eventLink = $("<a>").attr("href", eventResult.url).appendTo(eventTitlePosition); //event link
+            var eventTitle = $("<p>").addClass("title").addClass("is-4").text(eventResult.title).attr("id", "eventTitle").appendTo(eventLink); //event title
+            var eventVenue = $("<div>").addClass("content").text(eventResult.venue_name).attr("id", "eventVenue").appendTo(eventContent); //event venue
+            var eventTime = $("<div>").addClass("content").text(eventResult.start_time).attr("id", "eventTime").appendTo(eventContent); //event time
+            $("#resultsContainer").prepend(eventCard);
 
         };
-    });
-    // url: "https://app.ticketmaster.com/discovery/v2/events.json?keyword=music&source=universe&countryCode=US&city=Richmond&apikey=LoeeX4dO34SV1Xl7x72AzwTzSkHRVG0u",
-
-    $.ajax({
-        url: ticketmaster.queryURL("music"),
-        async: true,
-        dataType: "json",
-        method: "GET"
-    }).then(function(response){
-        console.log(response);
-        // for (var i = 0; i<response.events.event.length; i++) {
-        //     // console logs titles of events
-        //     console.log(response.events.event[i].title);
-        // };
     });
 });
